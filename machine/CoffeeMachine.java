11d19b946520bdfe66beb49eb71dfb38512a397a
cross.tamomo <cross.tamomo@gmail.com>
2024-09-14 20:21:50 -0500

    finilize coffee estimation feature

--- machine/CoffeeMachine.java
+++ machine/CoffeeMachine.java
@@ -9,28 +9,6 @@ public class CoffeeMachine {
         final int COFFEE_BEANS = 15;
         Scanner userInput = new Scanner(System.in);
 
-         System.out.println(
-                """
-                Starting to make a coffee
-                Grinding coffee beans
-                Boiling water
-                Mixing boiled water with crushed coffee beans
-                Pouring coffee into the cup
-                Pouring some MILK into the cup
-                Coffee is ready!""");
-
-        System.out.println("Write how many cups of coffee you will need:");
-        int cupsOfCoffee = userInput.nextInt();
-        System.out.printf(
-                "For %d cups of coffee you will need:%n" +
-                "%d ml of water%n" +
-                "%d ml of MILK%n" +
-                "%d g of coffee beans%n",
-                cupsOfCoffee,
-                WATER * cupsOfCoffee,
-                MILK * cupsOfCoffee,
-                COFFEE_BEANS * cupsOfCoffee);
-
         // User Input
         System.out.println("Write how many ml of water the coffee machine has:");
         int containsWater = userInput.nextInt();
@@ -47,7 +25,7 @@ public class CoffeeMachine {
         int maxCupsByCoffeeBeans = containsCoffeeBeans / COFFEE_BEANS;
         int maxCupsPossible = Math.min(maxCupsByWater, Math.min(maxCupsByMilk, maxCupsByCoffeeBeans));
 
-        // Checks how many cups of coffee can be mades
+        // Checks how many cups of coffee can be made
         if (containsWater >= coffeeAmount * WATER && containsMilk >= coffeeAmount * MILK && containsCoffeeBeans >= coffeeAmount * COFFEE_BEANS) {
             if (maxCupsPossible > coffeeAmount) {
                 System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)%n", maxCupsPossible - coffeeAmount);
