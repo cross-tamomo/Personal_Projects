64a3157d2fbb27b1b07c91536319f9ae4341e4bd
cross.tamomo <cross.tamomo@gmail.com>
2024-09-14 20:17:27 -0500

    add interation code-block

--- machine/CoffeeMachine.java
+++ machine/CoffeeMachine.java
@@ -9,7 +9,7 @@ public class CoffeeMachine {
         final int COFFEE_BEANS = 15;
         Scanner userInput = new Scanner(System.in);
 
-        System.out.println(
+         System.out.println(
                 """
                 Starting to make a coffee
                 Grinding coffee beans
@@ -24,13 +24,38 @@ public class CoffeeMachine {
         System.out.printf(
                 "For %d cups of coffee you will need:%n" +
                 "%d ml of water%n" +
-                "%d ml of MILK %n" +
-                "%d g of coffee beans",
+                "%d ml of MILK%n" +
+                "%d g of coffee beans%n",
                 cupsOfCoffee,
                 WATER * cupsOfCoffee,
                 MILK * cupsOfCoffee,
                 COFFEE_BEANS * cupsOfCoffee);
 
-        
+        // User Input
+        System.out.println("Write how many ml of water the coffee machine has:");
+        int containsWater = userInput.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int containsMilk = userInput.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int containsCoffeeBeans = userInput.nextInt();
+        System.out.println("Write how many cups of coffee you will need:");
+        int coffeeAmount = userInput.nextInt();
+
+        // Maximum cups of coffee that can be made
+        int maxCupsByWater = containsWater / WATER;
+        int maxCupsByMilk = containsMilk / MILK;
+        int maxCupsByCoffeeBeans = containsCoffeeBeans / COFFEE_BEANS;
+        int maxCupsPossible = Math.min(maxCupsByWater, Math.min(maxCupsByMilk, maxCupsByCoffeeBeans));
+
+        // Checks how many cups of coffee can be mades
+        if (containsWater >= coffeeAmount * WATER && containsMilk >= coffeeAmount * MILK && containsCoffeeBeans >= coffeeAmount * COFFEE_BEANS) {
+            if (maxCupsPossible > coffeeAmount) {
+                System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)%n", maxCupsPossible - coffeeAmount);
+            } else {
+              System.out.println("Yes, I can make that amount of coffee");
+            }
+        } else {
+            System.out.printf("No, I can only make %d cup(s) of coffee%n", maxCupsPossible);
+        }
     }
 }
